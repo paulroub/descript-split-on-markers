df15a9f8480241b6214db5138c13a320078237f7
Paul Roub <paul@roub.net>
2024-05-14 21:17:58 -0400

    pass file name test for single-file timeline

--- timeline/test_timeline.py
+++ timeline/test_timeline.py
@@ -12,3 +12,9 @@ def test_full_has_two_clips():
     clips = timeline.get_clips(xml_fn)
 
     assert len(clips) == 2
+
+def test_correct_source_file_name():
+    xml_fn = 'test_data/full.xml'
+    clips = timeline.get_clips(xml_fn)
+
+    assert clips[0]['source_file'] == 'Superman.mp4'
--- timeline/timeline.py
+++ timeline/timeline.py
@@ -4,6 +4,16 @@ import xml.etree.ElementTree as ET
 def get_clips(xml_fn):
     tree = ET.parse(xml_fn)
 
+    files = tree.findall('.//file/pathurl')
     markers = tree.findall('.//marker')
 
-    return markers[:-1]
+    clips = []
+
+    for marker in markers[:-1]:
+        clip = {
+            'source_file': files[0].text,
+        }
+
+        clips.append(clip)
+
+    return clips
