016731b02def79b419f743858d95600f8998b7b4
Paul Roub <paul@roub.net>
2024-08-02 16:02:50 -0400

    WIP

---
+++ timeline_cuts.py
@@ -0,0 +1,93 @@
+import datetime
+import json
+import locale
+import os
+import subprocess
+import sys
+
+from timeline import timeline
+
+SECONDS = 1
+
+
+def secs_to_time_str(secs):
+    return str(datetime.timedelta(seconds=secs))
+
+
+if len(sys.argv) != 1:
+    print("Usage: python timeline_cuts.py <timeline_file>")
+    sys.exit(1)
+
+timeline_fn = sys.argv[1]
+dest_dir = os.path.dirname(timeline_fn)
+
+if not os.path.isfile(timeline_fn):
+    print(f"Error: Timeline file {timeline_fn} does not exist.")
+    sys.exit(1)
+
+clips = timeline.get_clips(timeline_fn)
+
+chapters = []
+
+for clip in clips:
+    part_files = []
+    part_count = 0
+
+    for part in clip["parts"]:
+        part_count += 1
+
+        part_fn = f"part_{part_count}.mp4"
+        
+
+
+
+for chapter in chapter_list["chapters"][1:]:
+    new_chapter = {
+        "title": chapter["tags"]["title"],
+        "start": locale.atof(chapter["start_time"]),
+        "end": 0,
+    }
+
+    chapters.append(new_chapter)
+
+for i in range(len(chapters) - 1):
+    chapters[i]["end"] = chapters[i + 1]["start"]
+
+real_chapters = chapters[:-1]
+processes = []
+
+
+for i, chapter in enumerate(real_chapters):
+    output_filename = os.path.join(dest_dir, f"{i + 1} - {chapter['title']}.mp4")
+
+    initial_start = chapter["start"]
+    initial_end = chapter["end"]
+    padded_start = initial_start - PADDING_SECONDS
+    padded_end = initial_end + PADDING_SECONDS
+
+    silence_1_start = padded_start
+    silence_1_end = initial_start
+    silence_2_start = initial_end
+    silence_2_end = padded_end
+
+    af_param = f"volume=enable='between(t,{silence_1_start},{silence_1_end})':volume=0,afade=type=out:start_time={silence_2_start}:duration={PADDING_SECONDS}"
+
+    command = [
+        "ffmpeg",
+        "-hide_banner",
+        "-loglevel",
+        "error",
+        "-y",
+        "-i",
+        video_fn,
+        "-ss",
+        secs_to_time_str(padded_start),
+        "-to",
+        secs_to_time_str(padded_end),
+        "-af",
+        af_param,
+        output_filename,
+    ]
+
+    print(output_filename)
+    subprocess.run(command, check=True)
